5324c6ea742a41a484c67fcb09520e5bc072ce40
Eph Arceo <rerem456@gmail.com>
2023-09-19 11:50:40 +0800

    Revising Header name to Mance

--- dist/inventory-table.php
+++ dist/inventory-table.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventory - Mence Bicycle Shop</title>
+    <title>Inventory - Mance Bicycle Shop</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
--- dist/sales-point.php
+++ dist/sales-point.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Point Of Sales - Mence Bicycle Shop</title>
+    <title>Point Of Sales - Mance Bicycle Shop</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
